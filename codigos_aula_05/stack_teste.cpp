e4908a92bf7f416acfd8cb06d00d6efdf02d4db7
Alessandro de Almeida <agostini55@gmail.com>
2023-11-19 09:30:41 -0300

    Add files via upload

---
+++ codigos_aula_05/stack_teste.cpp
@@ -0,0 +1,29 @@
+#include "stack.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ItemType character;
+  Stack stack;
+  ItemType stackItem;
+  
+  cout << "Adicione uma String." << endl;     
+  cin.get(character);
+  while (character != '\n')
+    {
+      stack.push(character);
+      cin.get(character);
+    }
+  
+  while (!stack.isEmpty())
+    {
+      stackItem = stack.pop();
+      cout << stackItem;
+    }
+  
+  cout << endl;
+}
+
+// compilar: g++ stack_teste.cpp stack_vetor.cpp -o main
+// executar: ./main
