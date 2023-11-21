c1320a346c5dccf395d0abb0438a5f8defd80c70
Alessandro de Almeida <agostini55@gmail.com>
2023-11-21 08:57:04 -0300

    Add files via upload

---
+++ Estrutura-de-Dados-Tabela-Hash/aluno.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Aluno{
+    private:
+    int ra;
+    string nome;
+
+    public:
+    Aluno();
+    Aluno(int r, string n);
+    int obterRa();
+    string obterNome();
+};
